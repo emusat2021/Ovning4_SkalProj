0a194a92c98366a9c32675b493c494ef6a509715
Eugenia <musat.eugenia@gmail.com>
2024-03-15 16:43:20 +0100

    Ex1- Add functionality to ExamineList()
    AnswerToQuestions in .md doc and add images

--- AnswerToQuestions.md
+++ AnswerToQuestions.md
@@ -28,3 +28,18 @@ The difference between then:
 - In the method ReturnValue() there are two value types and then when y is assign another the value 4, x is having the same value of 3;
 - In the method ReturnValue2() there are two objects, different objects, and they are both reference types.
 Object y assign the value of object x. For this reason when object y assign a new value of 4 then the value on the heap is changing for both objects x and y.
+
+## Exercise 1) ExamineList() Answers:
+
+2. Capacity is increasing with the first item added to the list.
+3. Capacity is increasing with 4.
+![Adding one item from List - Capacity is 4](images/firstCapacityIncreasing.jpg)
+
+4. Count is always less than Capacity. While adding the items, if Count exceeds Capacity then the Capacity will increase automatically.
+5. Capacity is not decreasing when removing items from the list.
+![Removing item from List - Capacity is the same](images/sameCapacity.jpg)
+
+6. To use an array you must know from the begining the size of it.
+
+
+
--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -71,13 +72,46 @@ namespace SkalProj_Datastrukturer_Minne
              * As a default case, tell them to use only + or -
              * Below you can see some inspirational code to begin working.
             */
+            List<string> theList = new List<string>();
+            bool notToMainMenu = true;
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            while (notToMainMenu)
+            {
+                //sub menu
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Please navigate through the menu below:"
+                    + "\n+<Name>.   Add name in List (example: +Adam will add Adam to the List)"
+                    + "\n-<Name>.   Remove name in List (example: -Adam will remove Adam from the List)"
+                    + "\nq.         Go back to Main Menu");
 
-            //switch(nav){...}
+                string input = Console.ReadLine().ToLower();
+                char nav = input[0];
+                string value = input.Substring(1);
+
+                // Add name on the List respective remove as the user will have the posibility to choose
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        //feedback to the user after adding an item on the list
+                        PrintListInfo(theList);
+                        continue;
+                    case '-':
+                        theList.Remove(value);
+                        //feedback to the user after removing an item from the list
+                        PrintListInfo(theList);
+                        continue;
+                    case 'q':
+                        notToMainMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose only + or only -");
+                        continue;
+                }
+
+            }
+            //the final feedback to the user after the user will go out from the sub menu
+            PrintListInfo(theList);
         }
 
         /// <summary>
@@ -112,6 +146,18 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
 
+        }
+        public static void PrintListInfo(List<string> theList)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("The List includes the below names:");
+            foreach (string str in theList)
+            {
+                Console.WriteLine($"Name: {str}");
+            }
+            Console.WriteLine($"Number of items in the List: {theList.Count}");
+            Console.WriteLine($"Capacity of the List: {theList.Capacity}");
+            Console.ResetColor();
         }
 
     }
